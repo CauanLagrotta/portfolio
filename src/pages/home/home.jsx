480d0d61bd48830e18784efbf21dd3fe75683da0
CauanLagrotta <caun06lagrotta@gmail.com>
2024-10-03 21:12:49 -0300

    ajustes no header e continuando a home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -13,7 +13,9 @@ export function Home(){
             <RightSide>
                 <Title> {cauan} </Title>
 
-                <Text> Olá! Eu sou Cauan Lagrotta, desenvolvedor web em formação, apaixonado por criar experiências interativas e funcionais na web. Estou focado em aprender e aplicar as melhores práticas de desenvolvimento fullstack para construir interfaces dinâmicas e sistemas eficientes. </Text>
+                <Text>
+                     Olá! Eu sou Cauan Lagrotta, desenvolvedor web em formação, apaixonado por criar experiências interativas e funcionais na web. Estou focado em aprender e aplicar as melhores práticas de desenvolvimento fullstack para construir interfaces dinâmicas e sistemas eficientes. 
+                </Text>
                 <Bottom/>
 
                 <TechsDiv>
