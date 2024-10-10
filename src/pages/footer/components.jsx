d115dc67078bf360dc5eb1ef0463ac74bced0378
CauanLagrotta <caun06lagrotta@gmail.com>
2024-10-10 17:04:29 -0300

    footer feito

--- src/pages/footer/components.jsx
+++ src/pages/footer/components.jsx
@@ -7,8 +7,80 @@ const ContainerFooter = styled.div({
     color: "#fff",
 
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
+    gap: "50px",
+    padding: "0 10%",
+
+    '@media (max-width: 768px)': {
+        flexDirection: "column",
+        height: "auto",
+        padding: "20px 0",
+    }
 });
 
-export { ContainerFooter }
+const LeftSide = styled.div({
+    display: "flex",
+    alignItems: "center",
+    gap: "20px",
+
+    '@media (max-width: 768px)': {
+        flexDirection: "column",
+        gap: "10px",
+    }
+});
+
+const RightSide = styled.div({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    fontFamily: "Roboto, sans-serif",
+
+    '@media (max-width: 768px)': {
+        flexDirection: "column",
+        gap: "10px",
+    }
+});
+
+const LinkItem = styled.a({
+    fontSize: "20px",
+    color: "#fff",
+    textDecoration: "none",
+    fontFamily: "Roboto, sans-serif",
+
+    '&:hover': {
+        color: "#0e76a8",
+        textDecoration: "underline",
+        cursor: "pointer",
+    },
+
+    '@media (max-width: 768px)': {
+        fontSize: "18px",
+    }
+});
+
+const ContactItems = styled.div({
+    display: "flex",
+    alignItems: "center",
+    gap: "15px",
+
+    '@media (max-width: 768px)': {
+        gap: "10px",
+    }
+});
+
+const IconLink = styled.a({
+    fontSize: "30px",
+    color: "#fff",
+
+    '&:hover': {
+        color: "#0e76a8",
+        cursor: "pointer",
+    },
+
+    '@media (max-width: 768px)': {
+        fontSize: "25px",
+    }
+});
+
+export { ContainerFooter, LeftSide, RightSide, LinkItem, ContactItems, IconLink };
--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -1,9 +1,44 @@
-import { ContainerFooter } from "./components"
+import { ContainerFooter, LeftSide, RightSide, LinkItem, ContactItems, IconLink } from "./components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import Tooltip from "@mui/material/Tooltip";
 
-export function Footer(){
-    return(
+export function Footer() {
+    return (
         <ContainerFooter>
-            <h1>Footer</h1>
+            <LeftSide>
+                <LinkItem href="#home">Home</LinkItem>
+                <LinkItem href="#about">Sobre Mim</LinkItem>
+                <LinkItem href="#projects">Projetos</LinkItem>
+
+            </LeftSide>
+
+            <RightSide>
+                <span>Contato:</span>
+                <ContactItems>
+                    <Tooltip title="Email">
+                        <IconLink href="mailto:seu-email@dominio.com" target="_blank">
+                            <FontAwesomeIcon icon={faEnvelope} style={{ color: "#db4437" }} />
+                        </IconLink>
+                    </Tooltip>
+                    <Tooltip title="GitHub">
+                        <IconLink href="https://github.com/seu-usuario" target="_blank">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </IconLink>
+                    </Tooltip>
+                    <Tooltip title="LinkedIn">
+                        <IconLink href="https://linkedin.com/in/seu-usuario" target="_blank">
+                            <FontAwesomeIcon icon={faLinkedin} style={{ color: "#0e76ae" }} />
+                        </IconLink>
+                    </Tooltip>
+                    <Tooltip title="WhatsApp">
+                        <IconLink href="https://wa.me/seu-numero" target="_blank">
+                            <FontAwesomeIcon icon={faWhatsapp} style={{ color: "#25d366" }} />
+                        </IconLink>
+                    </Tooltip>
+                </ContactItems>
+            </RightSide>
         </ContainerFooter>
-    )
+    );
 }
