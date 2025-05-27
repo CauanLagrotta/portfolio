6425b936063d988958befbf55b54145b0df72087
CauanLagrotta <caun06lagrotta@gmail.com>
2025-05-26 23:13:24 -0300

    build: configurações iniciais

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/common/Header";
+
+export function App(){
+  return(
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      
+      <Header />
+    </div>
+  )
+}
